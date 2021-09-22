f97c6d27eb4305992b85dc58f75d839c7e782153
Ballonga03 <90762321+Ballonga03@users.noreply.github.com>
2021-09-22 11:57:30 +0200

    Add files via upload

---
+++ saludador.py
@@ -0,0 +1,5 @@
+def saludador():
+    nombre = raw_input("como te llamas?")
+    print("buenos dias " + nombre)
+
+saludador()
---
+++ saludador_2.py
@@ -0,0 +1,10 @@
+def saludador_2():
+    nombre = raw_input("como te llamas?")
+    edad = input("cuantos anios tienes?")
+    lugar = raw_input("donde vives?")
+    print("buenos dias " + nombre)
+    print("tienes " + str(edad) + " anios")
+    print("y se que vives en "+ lugar)
+    print("ten cuidado")
+
+saludador_2()
