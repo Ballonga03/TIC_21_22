3e0f59469a934ed17111d11df7c30cbcee459c97
Ballonga03 <90762321+Ballonga03@users.noreply.github.com>
2021-10-18 09:59:22 +0200

    Add files via upload

---
+++ bucle_while_4.py
@@ -0,0 +1,7 @@
+def tabla_multiplicar_while():
+    cont=99
+    while(cont>0):
+        print("numero "+ str(cont))
+        cont=cont-9
+    
+tabla_multiplicar_while()
---
+++ tabla_multiplicar_while.py
@@ -0,0 +1,9 @@
+def tabla_multiplicar_while():
+    numero=input("Que tabla deseas: ")
+    print("TABLA DEL "+¡ str(numero))
+    cont=0
+    while(cont>0):
+        print(numero +" x "+ str(cont)+ " = "+ str(numero*cont)
+    cont=cont+1
+    
+tabla_multiplicar_while()
