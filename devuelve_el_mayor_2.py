71c73e78122d15fa9b3c79acbdbc2a07d1ed774a
Ballonga03 <90762321+Ballonga03@users.noreply.github.com>
2021-11-15 09:59:16 +0100

    Add files via upload

---
+++ devuelve_el_mayor_2.py
@@ -0,0 +1,9 @@
+def devuelveme_el_mayor_2(num1,num2,num3):
+    num1=input("Introduce un numero: ")
+    num2=input("Introduce otro numero: ")
+    num3=input("Dame un tercer numero: ")
+    if(num1>num2):
+        mayor=1
+    if(num2>num3):
+        mayor=2
+    
---
+++ el meyor de los 2.py
@@ -0,0 +1,11 @@
+def devuelve_el_mayor(num1,num2):
+    num=0
+    while(mayor==num):
+        num1=input("Introduce un numero: ")
+        num2=input("Introduce otro numero: ")
+        if(num1>num2):
+            mayor=num1
+        else:
+            mayor=num2
+    return(mayor)
+devuelve_el_mayor
