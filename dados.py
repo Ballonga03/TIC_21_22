7c640bbfb9f15bbc18caad12057f9fb69d4af689
Ballonga03 <90762321+Ballonga03@users.noreply.github.com>
2021-11-22 10:35:32 +0100

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,27 @@
+import random
+
+def dados():
+    """Introduzca el nombre de los jugadores
+    PARTIDAS
+    Turno 1
+    Jugador 1
+    tirada 1= 6
+    Jugador 2
+    tirada 1= 3
+    [...]
+    RESULTADOS
+    Puntos jugador 1=
+    Puntos jugador 2=
+    Ha ganado jugador 2"""
+    print("Introduce el nombre de los jugadores")
+    player1=raw_input("PLAYER ONE:")
+    player2=raw_input("PlAYER TWO:")
+    puntos_1=0
+    puntos_2=0
+    while():
+        print("TURNO 1")
+        tirada=random.randint()
+        print("tirada 1 "+player1+" = "+tirada)
+        puntos_1=puntos_1+tirada 
+        
+    
