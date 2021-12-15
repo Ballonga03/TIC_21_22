8045c04fe6f0e3e752e7dc5fb45c29d613ec70b7
Ballonga03 <90762321+Ballonga03@users.noreply.github.com>
2021-12-15 11:23:11 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+//Esto es un comentario
+/*Esto es un comentariode 
+mas lineas*/
+//INSTRUCCIONES AL PREPROCESADOR
+#include<stdio.h>//FICHERO DE CABECERA
+int main(){
+	//DECLARACIÓN DE VARIABLES
+	char salir;
+	printf("Hola mundo, cruel");
+	printf("\n
+	PRESIONE CUALQUIER TECLA PARA SALIR: ");
+	scanf("%c",&salir);
+	return 0;
+
+
+}
